0e0ad06eb57c862bbd4740897923ac3f91718aca
JRansom12 <52975612+JRansom12@users.noreply.github.com>
2019-07-24 14:56:42 -0500

    Add files via upload
    Created functions for each mode of travel.
    Created a master function to calculate miles remaining.
    Began a mile counter.

--- diceGame.js
+++ diceGame.js
@@ -1,137 +1,134 @@
-// let yourName = prompt("Welcome to the game Cross the Country!\nWhat is your name?");
-// let thingInNewYork = prompt("What would you like to see in New York City?");
-// let dreamCar = prompt("What is your dream car?");
-// let horseName = prompt("What name do you think is a really cool name?");
-// let firstRoll = alert("Welcome to the game Cross the Country! You're in San Francisco at the airport going to New York City. You've lost all your belongings but you still have a ticket. Roll to see which city your ticket flies to. Can you make it all the way to NYC?");
+let userName = prompt("Welcome to the game Cross the Country!\nWhat is your name?");
+let dreamCar = prompt("What is your dream car?");
+let horseName = prompt("What name do you think is a really cool name?");
+let firstRoll = alert(userName + ", welcome to Cross the Country! You're in San Francisco at the airport and you must go to New York City. You've lost all your belongings but you still have a ticket. Roll a 6 sided die to see which city your ticket flies to. Will you Cross the Country?");
 
 function rollDice(number) {
   	let roll = Math.floor((Math.random() * number) + 1);
   	return roll;
 }
 
+function crossTheCountry(){ //masterFunction
+  let milesRemain = planeRide() - carRide() - hitchHike() - walk() - trainRide() - bikeRide() - horseRide();
+    console.log(milesRemain);
+    return milesRemain;
+    if(milesRemain <= 0){
+      console.log("Congratulations! You've made it Cross the Country.");
+      alert("Congratulations! You've made it Cross the Country.");
+    }
+    else{
+      console.log("You failed to Cross the Country by " + milesRemain + " miles. Better luck next time.");
+      alert("You failed to Cross the Country by " + milesRemain + " miles. Better luck next time.");
+    }
+}
+console.log(crossTheCountry());
+
+function planeRide() {
 let planeTravel = rollDice(6);
-	// if(planeTravel == 1){ //Kansas City
- //  		milesRemain = 1192;
- //  		  		alert("You rolled a 1. You're in Kansas City. You have 1192 miles to go! Good luck, you're gonna need it.");
- //  		return milesRemain;
- //  	}
- //  	if(planeTravel == 2){ //Memphis
- //  		milesRemain = 1097;
- //  		  		alert("You rolled a 2. You're in Memphis. You have 1,097 miles to go! You're going to need some grit to make it.");
- //  		return milesRemain;
- //  	}
- //  	if(planeTravel == 3){ //St. Louis
- //  		milesRemain = 954;
- //  		  		alert("You rolled a 3. You're in the gateway to the West, St. Louis. You have 954 miles to go!");
- //  		return milesRemain;
- //  	}
- //  	if(planeTravel == 4){ //Nashville
- //  		milesRemain = 886;
- //  		  		alert("You rolled a 4. You're in Nashville. You have 886 miles of country to go!");
- //  		return milesRemain;
- //  	}
- //  	if(planeTravel == 5){ //Atlanta
- //  		milesRemain = 864;
- //  		  		alert("You rolled a 5. You land in Atanta with 864 miles remaining.");
- //  		return milesRemain;
- //  	}
- //  	if(planeTravel == 6){ //Chicago
- //  		milesRemain = 789;
- //  		  		alert("You rolled a 6. You're in the Windy City! Chicago is as close as you can be so far with 789 miles to go.")
- //  		return milesRemain;
- //  	}
-let milesRemain = 800
-console.log(planeTravel);
-console.log(milesRemain);	
+	if(planeTravel == 1){ //Kansas City
+  		milesRemain = 1192;
+  		  		alert("You rolled a 1. You're in Kansas City. You have 1192 miles to go! Good luck, you're gonna need it.");
+  		return milesRemain;
+  	}
+  	if(planeTravel == 2){ //Memphis
+  		milesRemain = 1097;
+  		  		alert("You rolled a 2. You're in Memphis. You have 1,097 miles to go! You're going to need some grit to make it.");
+  		return milesRemain;
+  	}
+  	if(planeTravel == 3){ //St. Louis
+  		milesRemain = 954;
+  		  		alert("You rolled a 3. You're in the gateway to the West, St. Louis. You have 954 miles to go!");
+  		return milesRemain;
+  	}
+  	if(planeTravel == 4){ //Nashville
+  		milesRemain = 886;
+  		  		alert("You rolled a 4. You're in Nashville. You have 886 miles of country to go!");
+  		return milesRemain;
+  	}
+  	if(planeTravel == 5){ //Atlanta
+  		milesRemain = 864;
+  		  		alert("You rolled a 5. You land in Atanta with 864 miles remaining.");
+  		return milesRemain;
+  	}
+  	if(planeTravel == 6){ //Chicago
+  		milesRemain = 789;
+  		  		alert("You rolled a 6. You're in the Windy City! Chicago is as close as you can be so far with 789 miles to go.")
+  		return milesRemain;
+  	}
+}
+console.log(planeRide());
 
-let carTravel = rollDice(20);
-let gasTank = 10 + (1/2) * carTravel;
-let milesTravCar = 30 * gasTank;
-let milesRemain1 = milesRemain - milesTravCar;
-console.log(carTravel);
-console.log(milesRemain1);
+alert("Your ticket comes with a car rental. You rent a " + dreamCar +"! Roll to see how many gallons are in the tank.");
 
-let hitchHikeTravel = rollDice(6);
-let milesTravHitchHike = 180 + 30 * hitchHikeTravel;
-let milesRemain2 = milesRemain1 - milesTravHitchHike;
-console.log(hitchHikeTravel);
-console.log(milesRemain2);
-  if(milesRemain2 < 0){
-    console.log("Congratulations! You've made it Cross the Country.");
+function carRide(){
+    let carRoll = rollDice(20);
+    let gasTank = 10 + (1/2) * carRoll;
+    let milesTravCar = 30 * gasTank;
+        console.log(carRoll);
+        return milesTravCar;
+    alert("You rolled a " + carRoll + ". You've got " + gasTank + " gallons in the tank. You can drive " + milesTravCar + " miles before you run out of gas.");
+}
+console.log(carRide());
+
+alert("You're out of gas. As you walk away your " + dreamCar + " gets smaller each step you take. You put your thumb up; a semi-truck picks you up right away. The driver says he will drive as long as you can tell him some good stories. Roll to see how far you make it.");
+
+function hitchHike(){
+    let hitchHikeRoll = rollDice(6);
+    let milesTravHitchHike = 180 + 30 * hitchHikeRoll;
+        console.log(hitchHikeRoll);
+        return milesTravHitchHike;
+    alert("You rolled a " + hitchHikeRoll + ". He ends up driving you " + milesTravHitchHike + " miles before you're out of stories.");   
+}
+console.log(hitchHike());
+
+alert("You're walking now. How much energy do you have? Roll a 12 sided die to find out.");
+
+function walk(){
+    let walkRoll = rollDice(12);
+    let milesTravWalk = 3 * walkRoll;
+        console.log(walkRoll);
+        if(walkRoll <= 4){
+            alert("You rolled a " + walkRoll + ". You didn't have much energy. You walked " + milesTravWalk + " miles before getting exhausted.");
+}
+        else if(walkRoll > 4 && walkRoll <= 8){
+            alert("You rolled a " + walkRoll + ". You had a good amount of energy and walked " + milesTravWalk + " miles before getting exhausted.");
   }
-
-
-let walkTravel = rollDice(12);
-let milesTravWalk = 3 * walkTravel;
-let milesRemain3 = milesRemain2 - milesTravWalk;
-console.log(walkTravel);
-console.log(milesRemain3);
-if(milesRemain3 < 0){
-  console.log("Congratulations! You've made it Cross the Country.");
+        else{
+            alert("You rolled a " + walkRoll + "! You had gratuitous amounts of energy and walked " + milesTravWalk + " miles before getting exhausted.");
+  }
+    return milesTravWalk;
 }
+console.log(walk());
 
-let trainTravel = rollDice(8);
-let milesTravTrain = 20 * trainTravel;
-let milesRemain4 = milesRemain3 - milesTravTrain;
-console.log(trainTravel);
-console.log(milesRemain4);
-if(milesRemain4 < 0){
-  console.log("Congratulations! You've made it Cross the Country.");
+alert("You’re about to collapse when you hear a train. With one last burst of energy you throw yourself on a train car. Roll an 8 sided die to see how far the train travels before its next station.");
+
+function trainRide(){
+    let trainRoll = rollDice(8);
+    let milesTravTrain = 20 * trainRoll;
+        console.log(trainRoll);
+    return milesTravTrain;
+alert("You rolled a " + trainRoll + ". The train ends up going " + milesTravTrain + " miles. You hop off as the train grinds to a halt.");
 }
+console.log(trainRide());
 
-let bikeTravel = rollDice(10);
-let milesTravBike = 5 * bikeTravel;
-let milesRemain5 = milesRemain4 - milesTravBike;
-console.log(bikeTravel);
-console.log(milesRemain5);
-if(milesRemain5 < 0){
-  console.log("Congratulations! You've made it Cross the Country.");
+alert("You find a bicycle lying around. You’re feeling a bit rested from the train ride, maybe you can ride the rest of the way. You got this, " + userName + "!, you tell yourself. Roll a 10 sided die to determine how far you bike.");
+
+function bikeRide(){
+    let bikeRoll = rollDice(10);
+    let milesTravBike = 5 * bikeRoll;
+        console.log(bikeRoll);
+    return milesTravBike;
+alert("You rolled a " + bikeRoll + ". and you biked " + milesTravBike + " miles.");
 }
+console.log(bikeRide());
 
-let horseTravel = rollDice(4);
-let milesTravHorse = 10 * horseTravel
-let milesRemain6 = milesRemain5 - milesTravHorse
-console.log(horseTravel);
-console.log(milesRemain6);
+alert("Uh oh! You've got a flat tire. But there's a barn nearby and you can ride a horse! You name your horse " + horseName + " and saddle up. Roll a 4 sided die to see how far you ride.");
 
-if(milesRemain6 < 0){
-  console.log("Congratulations! You've made it Cross the Country.");
+function horseRide(){
+    let horseRoll = rollDice(4);
+    let milesTravHorse = 10 * horseRoll
+        console.log(horseRoll);
+    return milesTravHorse;
+alert("You rolled a " + horseRoll + ". You ride " + horseName + " " + milesTravHorse + " miles before your new horse needs a rest, and so do you.")
 }
-else{
-  console.log("You failed to Cross the Country, better luck next time.");
-}
-
-// function rollDice(number) {
-//   let roll = Math.floor((Math.random() * number) + 1);
-//   	if(roll == 1){ //Kansas City
-//   		milesRemain = 1192;
-//   		return milesRemain;
-//   	}
-//   	if(roll == 2){ //Memphis
-//   		milesRemain = 1097;
-//   		return milesRemain;
-//   	}
-//   	if(roll == 3){ //St. Louis
-//   		milesRemain = 954;
-//   		return milesRemain;
-//   	}
-//   	if(roll == 4){ //Nashville
-//   		milesRemain = 886;
-//   		return milesRemain;
-//   	}
-//   	if(roll == 5){ //Atlanta
-//   		milesRemain = 864;
-//   		return milesRemain;
-//   	}
-//   	if(roll == 6){ //Chicago
-//   		milesRemain = 789;
-//   		return milesRemain;
-//   	}
-// }
-// console.log(rollDice(6));
-
-// function rollDice(number) {
-//   let x = Math.floor((Math.random() * number) + 1);
-//   return x;
-//   }
-// console.log(rollDice(4));
+console.log(horseRide());
