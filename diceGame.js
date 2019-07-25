371f076a465e6f8093ff7f690139b29166af5d59
JRansom12 <52975612+JRansom12@users.noreply.github.com>
2019-07-25 07:58:38 -0500

    Add files via upload
    Game functionality needs improvement
    diceRoll function not in gameplay
    Needs improvement

--- diceGame.js
+++ diceGame.js
@@ -10,18 +10,12 @@ function rollDice(number) {
 
 function crossTheCountry(){ //masterFunction
   let milesToGo = planeRide() - carRide() - hitchHike() - walk() - trainRide() - bikeRide() - horseRide();
-    if(milesToGo <=0){
-      console.log("Congratulations! You've made it across the country.");
-      alert("Congratulations! You've made it across the country.");
-    }
-    else{
-      console.log("You failed to Cross the Country by " + milesRemain + " miles. Better luck next time.");
-      alert("You failed to Cross the Country by " + milesRemain + " miles. Better luck next time.");
-    }
-    return milesToGo;
-}
-console.log(crossTheCountry());
-
+      if(milesToGo <= 0){
+        alert("Congratulations! You crossed the country.");
+      }
+      else(milesToGo > 0)
+        alert("You failed to cross the country. You were " + milesToGo + " miles short. Better luck next time.");
+      }
 
 function planeRide() {
 let planeTravel = rollDice(6);
@@ -68,7 +62,6 @@ function carRide(){
         return milesTravCar;
     alert("You rolled a " + carRoll + ". You've got " + gasTank + " gallons in the tank. You can drive " + milesTravCar + " miles before you run out of gas.");
 }
-console.log(carRide());
 
 alert("You're out of gas. As you walk away your " + dreamCar + " gets smaller each step you take. You put your thumb up; a semi-truck picks you up right away. The driver says he will drive as long as you can tell him some good stories. Roll to see how far you make it.");
 
@@ -79,7 +72,6 @@ function hitchHike(){
         return milesTravHitchHike;
     alert("You rolled a " + hitchHikeRoll + ". He ends up driving you " + milesTravHitchHike + " miles before you're out of stories.");   
 }
-console.log(hitchHike());
 
 alert("You're walking now. How much energy do you have? Roll a 12 sided die to find out.");
 
@@ -98,7 +90,6 @@ function walk(){
             alert("You rolled a " + walkRoll + "! You had gratuitous amounts of energy and walked " + milesTravWalk + " miles before getting exhausted.");
   }
 }
-console.log(walk());
 
 alert("You're about to collapse when you hear a train. With one last burst of energy you throw yourself on a train car. Roll an 8 sided die to see how far the train travels before its next station.");
 
@@ -109,7 +100,6 @@ function trainRide(){
     return milesTravTrain;
 alert("You rolled a " + trainRoll + ". The train ends up going " + milesTravTrain + " miles. You hop off as the train grinds to a halt.");
 }
-console.log(trainRide());
 
 alert("You find a bicycle lying around. You're feeling a bit rested from the train ride, maybe you can ride the rest of the way. You got this, " + userName + "!, you tell yourself. Roll a 10 sided die to determine how far you bike.");
 
@@ -120,7 +110,6 @@ function bikeRide(){
     return milesTravBike;
 alert("You rolled a " + bikeRoll + ". and you biked " + milesTravBike + " miles.");
 }
-console.log(bikeRide());
 
 alert("Uh oh! You've got a flat tire. But there's a barn nearby and you can ride a horse! You name your horse " + horseName + " and saddle up. Roll a 4 sided die to see how far you ride.");
 
@@ -131,4 +120,5 @@ function horseRide(){
     return milesTravHorse;
 alert("You rolled a " + horseRoll + ". You ride " + horseName + " " + milesTravHorse + " miles before your new horse needs a rest, and so do you.")
 }
-console.log(horseRide());
+
+console.log(crossTheCountry());
